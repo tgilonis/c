d1bc9a5714b22c166b6713c719c9fe0a127717ac
tgilonis <tgilonis@protonmail.com>
2019-07-18 17:46:30 +0100

    second test commit

--- first_project/Pong/Pong/Pong.cpp
+++ first_project/Pong/Pong/Pong.cpp
@@ -106,11 +106,11 @@ private:
 	cPaddle* player1;
 	cPaddle* player2;
 public:
-	cGameManager(int h, int w) {
+	cGameManager(int w, int h) {
 		srand(time(NULL));
 		quit = false;
 		up1 = 'w'; up2 = 'i'; 
-		down1 = 's' down2 = 'k';
+		down1 = 's'; down2 = 'k';
 		score1 = score2 = 0;
 		width = w; height = h;
 		ball = new cBall(w / 2, h / 2);
@@ -132,11 +132,109 @@ public:
 		player1->Reset();
 		player2->Reset();
 	}
+	void Draw() {
+		system("cls");
+		//found characters through win+r "charmap" ->DOS western europe character set, then enter the code: 0xDB -> \xDB
+		for (int i = 0; i < width + 2; i++) cout << "\xB2";
+		cout << endl;
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				int ballx = ball->getX();
+				int bally = ball->getY();
+				int player1x = player1->getX();
+				int player2x = player2->getX();
+				int player1y = player1->getY();
+				int player2y = player2->getY();
+
+				if (j == 0) cout << "\xB2"; 
+
+				if (ballx == j && bally == i) cout << "O"; //ball
+				else if (player1x == j && player1y == i) cout << "\xDB"; //player 1
+				else if (player2x == j && player2y == i) cout << "\xDB"; //player 2
+
+				//extra paddle segments
+				else if (player1x == j && player1y + 1 == i) cout << "\xDB"; //player 1
+				else if (player1x == j && player1y + 2 == i) cout << "\xDB"; //player 1
+				else if (player1x == j && player1y + 3 == i) cout << "\xDB"; //player 1
+
+				else if (player2x == j && player2y + 1 == i) cout << "\xDB"; //player 1
+				else if (player2x == j && player2y + 2 == i) cout << "\xDB"; //player 1
+				else if (player2x == j && player2y + 3 == i) cout << "\xDB"; //player 1
+
+				
+
+				else cout << " ";
+
+				if (j == width - 1) cout << "\xB2";
+			}
+			cout << endl;
+		}
+		for (int i = 0; i < width + 2; i++) cout << "\xB2";
+		cout << endl;
+
+		cout << "Score 1: " << score1 << endl << "Score 2: " << score2 << endl;
+	}
+	void Input() {
+		ball->Move();
+		//can use helper function here
+		int ballx = ball->getX();
+		int bally = ball->getY();
+		int player1x = player1->getX();
+		int player2x = player2->getX();
+		int player1y = player1->getY();
+		int player2y = player2->getY();
+		if (_kbhit()) {
+			char current = _getch();
+			if (current == up1) if (player1y > 0) player1->moveUp();
+			if (current == up2) if (player2y > 0) player2->moveUp();
+
+			if (current == down1) if (player1y + 4 < height) player1->moveDown();
+			if (current == down2) if (player2y + 4 < height) player2->moveDown();
+
+			if (ball->getDirection() == STOP) ball->randomDirection();
+			if (current == 'q') quit = true;
+		}
+
+	}
+	
+	void Logic() {
+		int ballx = ball->getX();
+		int bally = ball->getY();
+		int player1x = player1->getX();
+		int player2x = player2->getX();
+		int player1y = player1->getY();
+		int player2y = player2->getY();
+
+		//left paddle
+		for (int i = 0; i < 4; i++) {
+			if (ballx == player1x + 1) if (bally == player1y + i) ball->changeDirection((eDir)((rand() % 3) + 4));
+		}
+		//right paddle
+		for (int i = 0; i < 4; i++) {
+			if (ballx == player2x - 1) if (bally == player2y + i) ball->changeDirection((eDir)((rand() % 3) + 1));
+		}
+		//bottom wall
+		if (bally == height - 1) ball->changeDirection(ball->getDirection() == DOWNRIGHT ? UPRIGHT : UPLEFT);
+		//top wall
+		if (bally == 0) ball->changeDirection(ball->getDirection() == UPRIGHT ? DOWNRIGHT : DOWNLEFT);
+		//right wall
+		if (ballx == width - 1) ScoreUp(player1);
+		//left wall
+		if (ballx == 0) ScoreUp(player2);
+	}
+	void Run() {
+		while (!quit) {
+			Draw();
+			Input();
+			Logic();			
+		}
+	}
 };
 
 int main()
 {
-	
+	cGameManager c(40, 20);
+	c.Run();
 	return 0;
 }
 
